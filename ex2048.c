eedb7220870f9df43c7c7e27b9258c7e8fffde54
Daidson Fonseca Alves <daidson.alves@gmail.com>
2017-06-08 19:24:02 -0300

    adicionado applyup no applymove() e criada a funcao applyup() ref #25

--- ex2048.c
+++ ex2048.c
@@ -181,6 +181,8 @@ int applymove(char m, int bd[BS][BS])
         return applyleft(bd);
     if (m == 'd')
         return applydown(bd);
+    if (m == 'u')
+        return applyup(bd);
     return 0;
 }
 int applyright(int bd[BS][BS])
@@ -317,6 +319,52 @@ int applydown(int bd[BS][BS])
 
     return a;
 }
+
+int applyup(int bd[BS][BS])
+{
+    int a = 0;      /* score */
+    int r, c;       /* rows and columns for loop */
+    int zerocount;  /* variable to store the amount of zeros scanned */
+    int last;       /* stores last valid scanned number */
+    
+    for (c = 0; c < BS; ++c)
+    {
+        zerocount = 0;  /* reset the count */
+        last = 0;       /* reset the last read number */
+        for (r = 0; r < BS; ++r)
+        {
+            if (bd[r][c])   /* if the tile is not empty */
+            {
+                if (bd[r][c] & last)    /* if the current number is fuse-able
+                                           with the last valid number(equal&not 0) */
+                {
+                    bd[r][c] *= 2;      /* doubles the tile's number to represent fusion */
+                    a += bd[r][c];      /* score setting */
+
+                    ++zerocount;        /* a fusion makes an empty slot, and this sets the
+                                           move code to replace the fused number */
+                    
+                    last = 0;           /* stops double-fusing by disabling fusions on the
+                                           next scan */
+                }
+                else
+                    last = bd[r][c];    /* update the last scan after a failed fusion */
+                
+                if (zerocount)          /* if there are zeroes before the current number
+                                           it needs to be moved */
+                {
+                    bd[r][c-zerocount] = bd[r][c];  /* moves the number to the last zero */
+                    bd[r][c] = 0;                   /* and replces the previous tile with 0 */
+                }
+            }
+            else
+                ++zerocount;            /* adds to the zero-counter after finding an empty tile */
+        }
+    }
+
+    return a;
+}
+
 /* ---------------------------------------------------------------------- */
 /* print the goodbye message and congratulations on the score */
 void goodbye(int score)
